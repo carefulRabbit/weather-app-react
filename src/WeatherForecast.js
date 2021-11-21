1394a65b02d6f0dab94f2ff78fce97111a3a612f
carefulRabbit <85527588+carefulRabbit@users.noreply.github.com>
2021-11-20 23:15:19 -0500

    Created single day of forecast
    Created single day of forecast. Made a component to handle forecast data display and formatting.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,35 +1,38 @@
 import axios from "axios";
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, {useState} from "react";
+import ForecastData from "./ForecastData";
+
 
 
 export default function WeatherForecast(props){
+    let [loaded, setLoaded] = useState(false);
+    let[forecast, setForecast] = useState(null);
+
     function handleResponse(response) {
-      console.log(response.data);
+      setForecast(response.data.daily);
+      setLoaded(true);
     }
     
-    let lat= props.coord.lat;
-    let lon = props.coord.lon;
-    let key = "a891fe6b8398b4aa610c0e82c0c5c1ee";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${key}&units=metric`;
 
-    axios.get(apiUrl).then(handleResponse);
-
-    
-    return(
-        <div className="WeatherForecast">
+    if(loaded){
+        return (
+          <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <div className="WeatherForecast-day">
-                        Thurs
-                    </div>
-                    <WeatherIcon code="10d" size={52}/>
-                    <div className="WeatherForecast-temps">
-                        <span >19</span>
-                        <span className="text-muted">10</span>
-                    </div>
-                </div>
+              <div className="col">
+                <ForecastData data={forecast[0]} />
+              </div>
             </div>
-        </div>
-    )
+          </div>
+        );
+    }
+    else{
+        let lat = props.coord.lat;
+        let lon = props.coord.lon;
+        let key = "a891fe6b8398b4aa610c0e82c0c5c1ee";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${key}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+        return(null);
+    }
+    
 }
